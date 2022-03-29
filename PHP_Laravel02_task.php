2844c25f7376a58e1d7de0f62c20680883aeb00c
Samurai_Walker <T6.0sa7i@gmail.com>
2022-03-29 14:04:21 +0000

    For PHP_Laravel02_task submission

---
+++ PHP_Laravel02_task.php
@@ -0,0 +1,26 @@
+<?php
+
+//課題１
+$a = 3;
+$b = 7;
+echo '課題１の回答:' . $a + $b;
+echo "\n";
+
+//課題２
+$array_month =['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'];
+echo '課題２の回答:' . $array_month[7];
+echo "\n";
+
+//課題３
+$hello = "Hello,";
+$name = "小山内徹";
+$world = "s World";
+echo '課題３の回答:' . $hello . $name . $world;
+echo "\n";
+
+
+//課題４
+$tech_boost = "tech";
+$tech_boost .= "boost";
+echo '課題４の回答:' . $tech_boost;
+echo "\n";
