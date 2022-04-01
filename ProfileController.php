e8d7a6e57216fa28dcfec10a9df8ffc096f307af
Samurai_Walker <T6.0sa7i@gmail.com>
2022-04-01 06:09:15 +0000

    PHP_Laravel08_Task4&5_submission

--- ProfileController.php
+++ ProfileController.php
@@ -7,5 +7,18 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    //PHP/Laravel08 ControllerとRoutingとの関係について 課題５
+    public function add()
+    {
+        return view('admin.profile.clreate');
+    }
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
+    public function edit()
+    {
+        return view('admin');
+    }
+
 }
